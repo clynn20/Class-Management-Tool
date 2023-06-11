a51188d1a4bd165a42e6552bb0a0c53927208d7a
clynn20 <lynnhu20@gmail.com>
2023-06-10 21:33:55 -0700

    User api endpoints

--- api/users.js
+++ api/users.js
@@ -1,79 +1,97 @@
 const { Router } = require('express');
 
-const { validateAgainstSchema } = require('../lib/validation')
-const {
-    UserSchema,
-    insertNewUser,
-    getUserById,
-    getUserByEmail,
-    validateUser
-} = require('../models/user')
-const { generateAuthToken, requireAuthentication } = require("../lib/auth")
+const { validateAgainstSchema, extractValidFields } = require('../lib/validation')
+
+const { UserSchema, LoginSchema, insertNewUser, getUserById, getUsers, validateUserEnP, getCourseTeachById } = require('../models/user')
+
+const { generateAuthToken, requireAuthenticationVer1, requireAuthenticationVer2 } = require("../lib/auth")
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
-    res.sendStatus(200);
+
+// use for quick peak what user we have in db, not the required api endpoint 
+router.get('/', async function (req, res, next) {
+    const results = await getUsers();
+    res.status(200).send(results);
 });
 
-router.post('/', async function (req, res) {
+
+// If not provide auth token, only can create normal user with student role
+// If provide valid admin auth token, can create all kinds of user 
+router.post('/', requireAuthenticationVer1, async function (req, res) {
     if (validateAgainstSchema(req.body, UserSchema)) {
-        const user = await getUserByEmail(req.body.email, false);
-        if (!user) {
-            if (req.body.role != "A" && req.body.role != "I") {
-                try {
-                    const id = await insertNewUser(req.body)
-                    res.status(201).send({ message: "New User successfully added.", _id: id })
-                } catch (e) {
-                    next(e)
-                }
-            }else{
-                if (req.isAdmin) {
-                    try {
-                        const id = await insertNewUser(req.body)
-                        res.status(201).send({ message: "New User successfully added.", _id: id })
-                    } catch (e) {
-                        next(e)
-                    }
-                }else{
-                    res.status(403).send({ message : "The request was not made by an authenticated User satisfying the authorization criteria described above."});
-                }
-            }
-        }else{
-            res.status(400).send({
-                error: "The request body was either not present or did not contain a valid User object."
+        const user = extractValidFields(req.body, UserSchema)
+        if (((user.role == "admin" || user.role == "instructor")) && (req.authUserRole != "admin")){
+            res.status(403).send({
+                error: "Didn't have valid user role, only admin user can create admin or instructor"
+            }) 
+        } else{
+            const id = await insertNewUser(user)
+            res.status(201).send({ 
+                _id: id,
+                links: `users/${id}`
             })
         }
     } else {
         res.status(400).send({
-            error: "The request body was either not present or did not contain a valid User object."
+            error: "The requested body is not present or did not contain a valid User object"
         })
     }
 })
 
+// user login endpoint, generate token for login user
 router.post('/login', async function (req, res, next) {
-    if (req.body && req.body.email && req.body.password) {
-        try {
-            const [authenticated, userId] = await validateUser(
-                req.body.email,
-                req.body.password
-            )
-            if (authenticated) {
-                const token = generateAuthToken(userId)
-                res.status(200).send({
-                    token: token
-                })
-            } else {
-                res.status(401).send({
-                    error: "Invalid authentication credentials"
-                })
-            }
-        } catch (e) {
-            next(e)
+    try{
+        if (validateAgainstSchema(req.body, LoginSchema)) {
+            const login = extractValidFields(req.body, LoginSchema)
+            const [authenticated, user] = await validateUserEnP(login.email, login.password)
+            console.log(authenticated)
+                if (authenticated) {
+                    const token = generateAuthToken(user._id)
+                    res.status(200).send({
+                        token: token
+                    })
+                } else {
+                    res.status(401).send({
+                        error: "Invalid authentication credentials"
+                    })
+                }
+        } else {
+            res.status(400).send({
+                error: "Request body requires email and password."
+            })
         }
-    } else {
-        res.status(400).send({
-            error: "Request body requires `id` and `password`."
+    } catch (err){
+        next(err)
+    }
+})
+
+
+//
+router.get('/:id', requireAuthenticationVer2, async function( req, res, next){
+    const id = req.params.id
+    const user = await getUserById(id, 1)
+    //console.log(user._id)
+    if(req.authUserId == id && user._id == id && user.role == "instructor"){
+        const courseTeach = await getCourseTeachById (id)
+        res.status(200).send({
+            user: user,
+            courses_list: courseTeach
+        })
+    }
+    // I'll go back to implement this after the post/courses/{id}/students is done
+    /*
+    else if (req.authUserId == id && user._id == id && user.role == "student"){
+        const courseEnroll = await getCourseEnrollById (id) 
+        res.status(200).send({
+            user: user, 
+            courses_list: courseEnroll
+        })
+    }
+    */ 
+    else{
+        res.status(403).send({
+            user: user
         })
     }
 })
--- models/user.js
+++ models/user.js
@@ -1,29 +1,38 @@
 const { ObjectId } = require('mongodb')
 const bcrypt = require("bcryptjs")
-
 const { extractValidFields } = require('../lib/validation')
-const { getDb } = require('../lib/mongo')
+const { getDbReference } = require('../lib/mongo')
 
 const UserSchema = {
-    Name: { type: String, required: true },
-    Email: { type: String, required: false },
-    Password: { type: String, required: true },
-    Role: { type: String, required: true }
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum:["admin", "instructor", "student"] }
+}
+exports.UserSchema = UserSchema
+
+const LoginSchema = {
+    email: {required: true },
+    password: {required: true}
+}
+exports.LoginSchema = LoginSchema
+
+// Get all users
+exports.getUsers = async function (){
+        const db = getDbReference()
+        const collection = db.collection('users')
+        const results = await collection.find().toArray()
+        return results
 }
 
-exports.UserSchema = UserSchema
 
 /*
  * Insert a new User into the DB.
  */
 exports.insertNewUser = async function (user) {
     const userToInsert = extractValidFields(user, UserSchema)
-
-    const hash = await bcrypt.hash(userToInsert.password, 8)
-    userToInsert.password = hash
-    console.log("  -- userToInsert:", userToInsert)
-
-    const db = getDb()
+    //console.log("  -- userToInsert:", userToInsert)
+    const db = getDbReference()
     const collection = db.collection('users')
     const result = await collection.insertOne(userToInsert)
     return result.insertedId
@@ -31,39 +40,54 @@ exports.insertNewUser = async function (user) {
 
 
 /*
- * Fetch a user from the DB based on user ID.
+ * Fetch a user from the DB by id.
  */
+
 async function getUserById (id, includePassword) {
-    const db = getDb()
-    const collection = db.collection('users')
-    if (!ObjectId.isValid(id)) {
-        return null
-    } else {
-        const results = await collection
-            .find({ _id: new ObjectId(id) })
-            .project(includePassword ? {} : { password: 0 })
-            .toArray()
-        return results[0]
-    }
+        const db = getDbReference()
+        const collection = db.collection('users')
+        const projection = includePassword? {} : { password: 0}
+        const result = await collection.find({ _id: new ObjectId(id)}).project(projection).toArray()
+        return result[0]
 }
 exports.getUserById = getUserById
 
 async function getUserByEmail (email, includePassword) {
-    const db = getDb()
+    const db = getDbReference()
     const collection = db.collection('users')
-    if (!ObjectId.isValid(id)) {
+    const projection = includePassword ? {} : { password: 0}
+    const result = await collection.find({ email: email }).project(projection).toArray()
+    if (result[0]){
+        return result[0]
+    }
+    else {
         return null
-    } else {
-        const results = await collection
-            .find({ Email: email })
-            .project(includePassword ? {} : { password: 0 })
-            .toArray()
-        return results[0]
     }
 }
 exports.getUserByEmail = getUserByEmail
 
-exports.validateUser = async function (email, password) {
-    const user = await getUserByEmail(email, true)
-    return [user && await bcrypt.compare(password, user.password), user._id]
+
+exports.validateUserEnP = async function (email, password) {
+    const user = await getUserByEmail(email, 1)
+    const authenticated = user && user.password == password
+    return [authenticated, user]
 }
+
+
+exports.getCourseTeachById = async function (id) {
+        const db = getDbReference()
+        const collection = db.collection('courses')
+        const results = await collection.find({ instructorId: id}).toArray()
+        if(results){
+            return results
+        } else{
+            return null
+        }
+}
+
+/*
+exports.getCourseEnrollById = async function (id) {
+    const db = getDbReference()
+    const collection = db.collection('courses')
+}
+*/
